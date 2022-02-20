11cd92d5608d7594ac66ae50087843efd36d6ef0
pg <pgenz10@gmail.com>
2022-02-20 19:28:34 +0100

    new update

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import "./style.css";
 
 import { PgGrid } from "./pg-grid/pg-grid";
+import { PgGridDataCtrl } from "./pg-grid/pg-grid-data-ctrl";
+import { PgGridCtrl } from "./pg-grid/pg-grid-ctrl";
 
 export interface test {
 	he: number;
@@ -12,7 +14,9 @@ const app = document.querySelector<HTMLDivElement>("#app")!;
 
 // init
 // try{
-let pgGrid = new PgGrid<test>("#app", app);
+let pgDataCtrl = new PgGridDataCtrl<test>();
+let pgCtrl = new PgGridCtrl<test>(app);
+// let pgGrid = new PgGrid<test>("#app", app);
 
 // }
 // catch(e){
